9fa26ab41bec71511faa36066896859730fc03db
True Grom <truegrom@gmail.com>
2019-03-20 01:50:37 +0300

    fix: Fix parallel document saving

--- utils/updates.js
+++ utils/updates.js
@@ -3,50 +3,35 @@ const logger = require("../logger");
 const MergeRequest = require("../models/merge-request");
 
 async function getApprovals(openedMergeRequests) {
-  const approvals = await Promise.all(openedMergeRequests.map(({ iid }) => api.getMergeRequestApprovals(iid)));
-  for (const approval of approvals) {
-    try {
-      const mergeRequest = openedMergeRequests.find(({ iid }) => approval.iid === iid);
-      await mergeRequest.setApprovals(approval.approved_by.map(({ user }) => user));
-    } catch (e) {
-      logger.error(e);
-    }
-  }
-  return openedMergeRequests;
+  return Promise.all(openedMergeRequests.map(({ iid }) => api.getMergeRequestApprovals(iid)));
 }
 
 async function getAwardEmojis(openedMergeRequests) {
-  const allEmojis = await Promise.all(openedMergeRequests.map(({ iid }) => api.getAwardEmojis(iid)));
-  for (const [index, emoji] of allEmojis.entries()) {
-    try {
-      const mergeRequest = openedMergeRequests[index];
-      await mergeRequest.setEmojis(emoji);
-    } catch (e) {
-      logger.error(e);
-    }
-  }
+  return Promise.all(openedMergeRequests.map(({ iid }) => api.getAwardEmojis(iid)));
 }
 
 async function getPipelines(openedMergeRequests) {
-  const allPipelines = await Promise.all(openedMergeRequests.map(({ iid }) => api.getPipelines(iid)));
-  for (const [index, pipeline] of allPipelines.entries()) {
-    try {
-      const mergeRequest = openedMergeRequests[index];
-      await mergeRequest.setPipelines(pipeline);
-    } catch (e) {
-      logger.error(e);
-    }
-  }
+  return Promise.all(openedMergeRequests.map(({ iid }) => api.getPipelines(iid)));
 }
 
 async function getMetaUpdates() {
   try {
     const openedMergeRequests = await MergeRequest.getOpened();
-    return Promise.all([
+    const [allApprovals, allEmojis, allPipelines] = await Promise.all([
       getApprovals(openedMergeRequests),
       getAwardEmojis(openedMergeRequests),
       getPipelines(openedMergeRequests)
     ]);
+    for (const [index, mergeRequest] of openedMergeRequests.entries()) {
+      try {
+        const approvals = allApprovals[index];
+        const emojis = allEmojis[index];
+        const pipelines = allPipelines[index];
+        await mergeRequest.setMetaData({ approvals, emojis, pipelines });
+      } catch (e) {
+        logger.error(e);
+      }
+    }
   } catch (e) {
     logger.error(e);
   }
